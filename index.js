229e449f726080ce2e6b9e10be242c0cda677018
jameslock86 <31215180+jameslock86@users.noreply.github.com>
2017-11-03 15:36:17 -0500

    stop light

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  // YOUR CODE HERE
+
+
+
+
+	let stop = document.getElementById('stopButton');
+	let stopR = document.getElementById('stopLight');
+	stop.addEventListener('click',function(){stopR.classList.toggle('stop');});
+
+	let slow = document.getElementById('slowButton');
+	let slowY = document.getElementById('slowLight');
+	slow.addEventListener('click',function(){slowY.classList.toggle('slow');});
+
+	let go = document.getElementById('goButton');
+	let goG = document.getElementById('goLight');
+	go.addEventListener('click',function(){goG.classList.toggle('go');});
+
+
+
+
+	// let slow = document.getElementById('slowButton');
+	// slow.classList.toggle('yellow');
+	// slow.classList.toggle('slow');
+	// let slowY = document.getElementById('controls');
+	// slowY.addEventListener('click', false);
+	//
+	//
+	// let go = document.getElementById('goButton');
+	// go.classList.toggle('green');
+	// go.classList.toggle('go');
+	// let goG = document.getElementById('controls');
+	// goG.addEventListener('click', false);
+
+
+
 })();
+
+
+
+// add event listener to table
